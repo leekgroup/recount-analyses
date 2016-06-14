06ea73fd9693ba2e216b74a8ac031db1e28ff84c
lcolladotor <lcollado@jhsph.edu>
2016-06-14 13:15:24 -0400

    Added code for generating exons coverage matrix for XIST for 2034 projects

--- example_xist/xist_mc.sh
+++ example_xist/xist_mc.sh
@@ -3,7 +3,7 @@
 #$ -m e
 #$ -l mem_free=1G,h_vmem=2G
 #$ -pe local 10
-#$ -N SRP032789_der
+#$ -N mc_xist
 
 echo "**** Job starts ****"
 date
@@ -12,10 +12,10 @@ mkdir -p logs
 
 ## Determine ERs
 module load R/3.3.x
-Rscript recount_DER_SRP032789_mc.R
+Rscript xist_mc.R
 
 # Move log files into the logs directory
-mv SRP032789_der.* logs/
+mv mc_xist.* logs/
 
 echo "**** Job ends ****"
 date
